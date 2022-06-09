6bd63bbd030897ce0608ee565663812c966d3e5a
VedangSardessai <vms112000@gmail.com>
2022-06-09 20:07:08 +0530

    Fixed screen to portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:cipher_gred/screens/wrapper.dart';
 import 'package:cipher_gred/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
 
   runApp(const MainClass());
 }
@@ -17,6 +19,10 @@ class MainClass extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return StreamProvider<Userr?>.value(
       catchError: (_, __) => null,
       initialData: null,
